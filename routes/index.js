3d16c48dc25d2788c6a7fa14ab3a6d6f35b595ac
haru5386 <snsn550@gmail.com>
2021-08-07 19:11:17 +0800

    feat: add register/login route/page

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
-router.use('/', home)
+router.use('/users', users)
 router.use('/restaurants', restaurants)
-
+router.use('/', home)
 
 module.exports = router
