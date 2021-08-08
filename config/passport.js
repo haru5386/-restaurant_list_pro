4119994641576f0c60332e8b52c49d8dee3b35d7
haru5386 <snsn550@gmail.com>
2021-08-08 15:24:48 +0800

    feat: add facebook login & dotenv

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,33 @@ const User = require('../models/user')
 module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
-
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+    User.findOne({ email })
+      .then(user => {
+        if (user) {
+          return done(null, user)
+        }
+        const randomPassword = Math.random().toString(36).slice(-8)
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => {
+            User.create({
+              name,
+              email,
+              password: hash
+            })
+          })
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+  }))
   passport.use(new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
